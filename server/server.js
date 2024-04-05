785af98f05b9418680bcd122c691531490ff6b6a
Saragol Barakchian <saragolbarakchian@wl-r5-223.rz.tu-ilmenau.de>
2024-04-05 21:06:43 +0200

    finish client side request.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-
 app.use(express.json());
+app.use(express.static("public"));
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
@@ -11,5 +11,9 @@ storeItems = new Map([
   [1, { priceInCents: 1000, name: "Learn React Today" }],
   [2, { priceInCents: 2000, name: "Learn CSS Today" }],
 ]);
+
+app.post("/create-checkout-session", (req, res) => {
+  res.json({ url: "Hi" });
+});
 
 app.listen(3000);
