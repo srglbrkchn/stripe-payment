9997689370aa6d3e1fcc169002eda22f7f17afd0
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-04-05 10:57:36 +0200

    finish up the boiling code of the server.

--- server/server.js
+++ server/server.js
@@ -4,3 +4,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+storeItems = new Map([
+  [1, { priceInCents: 1000, name: "Learn React Today" }],
+  [2, { priceInCents: 2000, name: "Learn CSS Today" }],
+]);
+
+app.listen(3000);
